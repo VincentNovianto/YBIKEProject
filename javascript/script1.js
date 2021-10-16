cc2add2540c4697dad1a97ad8926d96234bfda52
VincentNovianto <vincent.novianto@binus.ac.id>
2021-10-16 17:10:30 +0700

    first commit

---
+++ javascript/script1.js
@@ -0,0 +1,80 @@
+console.log("Connected");
+
+function SubmitForm(form){
+
+    console.log(form.nameN.value);
+    console.log(form.phoneN.value);
+    console.log(form.motorN.value);
+
+    if(!validateName(form.nameN.value)){
+        return false;
+    }
+
+    if(!validatePhone(form.phoneN.value)){
+        return false;
+    }
+
+    if(!validateMotor(form.motorN.value)){
+        return false;
+    }
+    
+    if(!validateQuantity(form.quantityN.value)){
+        return false;
+    }
+
+    return true;
+
+}
+
+
+
+function validateName(data){
+    if(data == ""){
+        alert("Input your Name")
+        return false;
+    }
+    return true;
+}
+
+function validatePhone(data){
+    if(data == ""){
+        alert("Input Number")
+        return false
+    }else{
+        
+        return true;
+    }
+}
+
+// function validateQuantity(data){
+//     if(data == ""){
+//         alert("Input your quantity")
+//         return false
+//     }else{
+        
+//         return true;
+//     }
+// }
+
+
+function validateMotor(data){
+    if(data == "Choose Your Varian"){
+        alert("gitu la")
+        return false;
+    }
+    return true;
+
+}
+
+function validateQuantity(data)
+{
+    var z = /^[0-9]+$/;
+    if(data.match(z))
+        {
+            return true
+        
+        }else{
+            alert("Please only enter numeric characters only for your Quantity! (Allowed input:0-9)")
+            return false
+        }
+}
